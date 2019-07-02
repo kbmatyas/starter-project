3764eaedaedd4ed67c0247a4ad7d2317602bcc74
Jake Langfeldt <jake.langfeldt@possible.com>
2019-07-02 13:54:33 -0600

    Add Retrofit and RxJava initialization with GitHub API

---
+++ app/src/main/java/com/possible/demo/GitHubResponse.kt
@@ -0,0 +1,8 @@
+package com.possible.demo
+
+import com.google.gson.annotations.SerializedName
+
+class GitHubResponse {
+    @SerializedName("login")
+    val login: String? = null
+}
--- app/src/main/java/com/possible/demo/MainActivity.kt
+++ app/src/main/java/com/possible/demo/MainActivity.kt
@@ -2,11 +2,55 @@ package com.possible.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    var disposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL_GITHUB)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        val gitHubService = retrofit.create(GitHubService::class.java)
+
+        disposable.add(gitHubService.getFollowers(USERNAME_GITHUB)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                        this@MainActivity::onSuccess,
+                        this@MainActivity::onError
+                ))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposable.clear()
+    }
+
+    fun onSuccess(followers: List<GitHubResponse>) {
+        followers.forEach {
+            Log.i(TAG_GITHUB, it.login)
+        }
+    }
+
+    fun onError(throwable: Throwable) {
+        Log.i(TAG_GITHUB, throwable.message)
+    }
+
+    companion object {
+        const val BASE_URL_GITHUB = "https://api.github.com/"
+        const val USERNAME_GITHUB = "jakelangfeldt"
+        const val TAG_GITHUB = "GitHub"
     }
 }
